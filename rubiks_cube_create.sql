189e48a74e90c1e7b3ff5e886d3f476409e396bb
agallier2 <69326055+agallier2@users.noreply.github.com>
2021-12-04 13:35:55 -0500

    New procedure

--- rubiks_cube_create.sql
+++ rubiks_cube_create.sql
@@ -2,28 +2,6 @@ DROP DATABASE IF EXISTS cubes;
 CREATE DATABASE cubes;
 USE cubes;
 
--- The user entity
-CREATE TABLE users
-(
-	`user_id`	INT 	PRIMARY KEY		auto_increment,
-    `username`	VARCHAR(100)	NOT NULL
-);
-
--- The session entity
-CREATE TABLE sessions
-(
-	`session_id` INT	PRIMARY KEY		auto_increment,
-    `session_name`	VARCHAR(100),
-    `start_time` 	DATETIME
-);
-
--- The solve entity
-CREATE TABLE solves
-(
-	`solve_id`	INT		PRIMARY KEY		auto_increment,
-    `time`		FLOAT
-);
-
 -- The cube type entity
 CREATE TABLE cube_types
 (
@@ -31,6 +9,34 @@ CREATE TABLE cube_types
     `cube_shape`	VARCHAR(100),
 	`cube_size`		INT,
     `name`		VARCHAR(100)
+);
+
+-- The user entity
+CREATE TABLE users
+(
+	`user_id`	INT 	PRIMARY KEY		auto_increment,
+    `username`	VARCHAR(100)	NOT NULL	unique
+);
+
+-- The session entity
+CREATE TABLE sessions
+(
+	`session_id` INT	PRIMARY KEY		auto_increment,
+    `session_name`	VARCHAR(100),
+    `start_time` 	DATETIME,
+    `cube_id`		INT,
+    CONSTRAINT session_fk_cube
+    FOREIGN KEY (cube_id)
+    REFERENCES cube_types (cube_id) -- put in session instead
+    ON UPDATE CASCADE ON DELETE SET NULL
+);
+
+-- The solve entity
+CREATE TABLE solves
+(
+	`solve_id`	INT		PRIMARY KEY		auto_increment,
+    `time`		FLOAT,
+    `penalty`	VARCHAR(32)
 );
 
 -- The round entity
@@ -43,11 +49,7 @@ CREATE TABLE rounds
     CONSTRAINT round_fk_session
     FOREIGN KEY (session_id)
     REFERENCES sessions (session_id)
-    ON UPDATE CASCADE ON DELETE CASCADE,
-    CONSTRAINT round_fk_cube
-    FOREIGN KEY (cube_id)
-    REFERENCES cube_types (cube_id)
-    ON UPDATE CASCADE ON DELETE SET NULL
+    ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 -- The complex relationship
@@ -76,18 +78,18 @@ CREATE TABLE solve_logs
 -- The relationship between users
 CREATE TABLE friends
 (
-	`id_1`		INT		NOT NULL,
-    `id_2`		INT		NOT NULL,
+	`user_1_id`		INT		NOT NULL,
+    `user_2_id`		INT		NOT NULL,
     CONSTRAINT friends_fk_1
-    FOREIGN KEY (id_1)
+    FOREIGN KEY (user_1_id)
     REFERENCES users (user_id)
     ON UPDATE CASCADE ON DELETE CASCADE,
     CONSTRAINT friends_fk_2
-    FOREIGN KEY (id_2)
+    FOREIGN KEY (user_2_id)
     REFERENCES users (user_id)
     ON UPDATE CASCADE ON DELETE CASCADE,
     CONSTRAINT friends_pk
-    PRIMARY KEY (id_1, id_2)
+    PRIMARY KEY (user_1_id, user_2_id)
 );
 
 INSERT INTO `users` (`username`) VALUES ('audrey');
@@ -97,16 +99,16 @@ INSERT INTO `users` (`username`) VALUES ('brooke');
 INSERT INTO `cube_types` (`cube_shape`, `cube_size`, `name`) VALUES ('cube', 3, 'standard');
 INSERT INTO `cube_types` (`cube_shape`, `cube_size`, `name`) VALUES ('cube', 4, '4 by 4');
 
-INSERT INTO `solves` (`time`) VALUES (34.5);
+INSERT INTO `solves` (`time`, `penalty`) VALUES (34.5, "DNF");
 INSERT INTO `solves` (`time`) VALUES (50.1);
 INSERT INTO `solves` (`time`) VALUES (100.4);
-INSERT INTO `solves` (`time`) VALUES (73.0);
+INSERT INTO `solves` (`time`, `penalty`) VALUES (73.0, "+2");
 INSERT INTO `solves` (`time`) VALUES (30.4);
 
-INSERT INTO `sessions` (`session_name`, `start_time`) VALUES ('session_1', '2021-12-04 13:23:44');
+INSERT INTO `sessions` (`session_name`, `start_time`, `cube_id`) VALUES ('session_1', '2021-12-04 13:23:44', 1);
 
-INSERT INTO `rounds` (`scramble`, `session_id`, `cube_id`) VALUES ("D' R2 U2 D2 B' U F D L2 B2 F' D F' B' R2 L B D L' B2 L U' D' B L2", 1, 1);
-INSERT INTO `rounds` (`scramble`, `session_id`, `cube_id`) VALUES ("R D B2 U' R2 F' D U2 F' L B L F D R2 D U' B' R' F2 R B' L U L", 1, 2);
+INSERT INTO `rounds` (`scramble`, `session_id`) VALUES ("D' R2 U2 D2 B' U F D L2 B2 F' D F' B' R2 L B D L' B2 L U' D' B L2", 1);
+INSERT INTO `rounds` (`scramble`, `session_id`) VALUES ("R D B2 U' R2 F' D U2 F' L B L F D R2 D U' B' R' F2 R B' L U L", 1);
 
 INSERT INTO `solve_logs` (`solve_id`, `user_id`, `round_id`) VALUES (1, 1, 1);
 INSERT INTO `solve_logs` (`solve_id`, `user_id`, `round_id`) VALUES (2, 2, 1);
@@ -114,5 +116,5 @@ INSERT INTO `solve_logs` (`solve_id`, `user_id`, `round_id`) VALUES (3, 3, 1);
 INSERT INTO `solve_logs` (`solve_id`, `user_id`, `round_id`) VALUES (4, 1, 2);
 INSERT INTO `solve_logs` (`solve_id`, `user_id`, `round_id`) VALUES (5, 3, 2);
 
-INSERT INTO `friends` (`id_1`, `id_2`) VALUES (1, 2);
-INSERT INTO `friends` (`id_1`, `id_2`) VALUES (1, 3);
+INSERT INTO `friends` (`user_1_id`, `user_2_id`) VALUES (1, 2);
+INSERT INTO `friends` (`user_1_id`, `user_2_id`) VALUES (1, 3);
--- rubiks_cube_functions_procedures.sql
+++ rubiks_cube_functions_procedures.sql
@@ -1,1 +1,32 @@
 USE cubes;
+
+DROP FUNCTION IF EXISTS get_user_id;
+DELIMITER //
+ CREATE FUNCTION get_user_id(username_arg VARCHAR(100))
+ RETURNS INT
+ DETERMINISTIC
+ READS SQL DATA
+ BEGIN
+	DECLARE result INT;
+    SELECT user_id INTO result FROM (SELECT * FROM users WHERE username = username_arg) AS id;
+    RETURN result;
+ END//
+DELIMITER ;
+
+-- Procedure to find all the friends of a user
+DROP PROCEDURE IF EXISTS find_friends;
+DELIMITER //
+CREATE PROCEDURE find_friends(IN uname VARCHAR(100))
+BEGIN
+-- WITH id AS (SELECT get_user_id(username))
+SELECT username FROM users JOIN
+((SELECT user_1_id AS this_user, user_2_id AS friend FROM friends WHERE user_1_id = (SELECT get_user_id(uname)))
+UNION (SELECT user_2_id AS this_user, user_1_id AS friend FROM friends WHERE user_2_id = (SELECT get_user_id(uname)))) AS friends
+ON friend = users.user_id;
+END //
+DELIMITER ;
+
+CALL find_friends('audrey');
+
+
+SELECT get_user_id('graham');
