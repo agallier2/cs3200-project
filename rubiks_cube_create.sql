7b71fdf3f4c75a6d1bd14c4ccbaa79a473b4ef80
Graham Preston <graham.preston@gmail.com>
2021-12-06 13:21:49 -0500

    Remove solve_logs from create script

--- rubiks_cube_create.sql
+++ rubiks_cube_create.sql
@@ -1,6 +1,6 @@
-DROP DATABASE IF EXISTS cubes1;
-CREATE DATABASE cubes1;
-USE cubes1;
+DROP DATABASE IF EXISTS cubes3;
+CREATE DATABASE cubes3;
+USE cubes3;
 
 -- The cube type entity
 CREATE TABLE cube_types
@@ -45,31 +45,18 @@ CREATE TABLE rounds
 CREATE TABLE solves
 (
 	`solve_id`	INT		PRIMARY KEY		auto_increment,
-    `time`		FLOAT,
-    `penalty`	VARCHAR(32)
-);
-
--- The complex relationship
--- Can change later
-CREATE TABLE solve_logs
-(
-	`solve_id`	INT 	NOT NULL	UNIQUE,
-    `user_id`	INT		NOT NULL,
-    `round_id`	INT		NOT NULL,
-    CONSTRAINT logs_fk_solve
-    FOREIGN KEY (solve_id)
-    REFERENCES solves (solve_id)
-    ON UPDATE CASCADE ON DELETE CASCADE,
-    CONSTRAINT logs_fk_user
+    `time`		FLOAT NOT NULL,
+    `penalty`	VARCHAR(32),
+	`user_id`  	INT NOT NULL,
+    `round_id`  INT NOT NULL,
+    CONSTRAINT solves_fk_users
     FOREIGN KEY (user_id)
     REFERENCES users (user_id)
     ON UPDATE CASCADE ON DELETE CASCADE,
-    CONSTRAINT logs_fk_round
+    CONSTRAINT solves_fk_rounds
     FOREIGN KEY (round_id)
     REFERENCES rounds (round_id)
-    ON UPDATE CASCADE ON DELETE CASCADE,
-    CONSTRAINT logs_pk
-    PRIMARY KEY (user_id, round_id)
+    ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 -- The relationship between users
@@ -128,24 +115,17 @@ INSERT INTO `cube_types` (`name`) VALUES ('3x3');
 INSERT INTO `cube_types` (`name`) VALUES ('4x4');
 INSERT INTO `cube_types` (`name`) VALUES ('Pyraminx');
 
-INSERT INTO `solves` (`time`, `penalty`) VALUES (34.5, "DNF");
-INSERT INTO `solves` (`time`) VALUES (50.1);
-INSERT INTO `solves` (`time`) VALUES (100.4);
-INSERT INTO `solves` (`time`, `penalty`) VALUES (73.0, "+2");
-INSERT INTO `solves` (`time`) VALUES (30.4);
-INSERT INTO `solves` (`time`) VALUES (90.9);
-
 INSERT INTO `sessions` (`session_name`, `cube_id`) VALUES ('session_1', 1);
 
 INSERT INTO `rounds` (`scramble`, `session_id`) VALUES ("D' R2 U2 D2 B' U F D L2 B2 F' D F' B' R2 L B D L' B2 L U' D' B L2", 1);
 INSERT INTO `rounds` (`scramble`, `session_id`) VALUES ("R D B2 U' R2 F' D U2 F' L B L F D R2 D U' B' R' F2 R B' L U L", 1);
 
-INSERT INTO `solve_logs` (`solve_id`, `user_id`, `round_id`) VALUES (1, 1, 1);
-INSERT INTO `solve_logs` (`solve_id`, `user_id`, `round_id`) VALUES (2, 2, 1);
-INSERT INTO `solve_logs` (`solve_id`, `user_id`, `round_id`) VALUES (3, 3, 1);
-INSERT INTO `solve_logs` (`solve_id`, `user_id`, `round_id`) VALUES (4, 1, 2);
-INSERT INTO `solve_logs` (`solve_id`, `user_id`, `round_id`) VALUES (5, 3, 2);
-INSERT INTO `solve_logs` (`solve_id`, `user_id`, `round_id`) VALUES (6, 4, 2);
+INSERT INTO `solves` (`time`, `penalty`, `user_id`, `round_id`) VALUES (34.5, "DNF", 1, 1);
+INSERT INTO `solves` (`time`, `user_id`, `round_id`) VALUES (50.1, 2, 1);
+INSERT INTO `solves` (`time`, `user_id`, `round_id`) VALUES (100.4, 3, 1);
+INSERT INTO `solves` (`time`, `penalty`, `user_id`, `round_id`) VALUES (73.0, "+2", 1, 2);
+INSERT INTO `solves` (`time`, `user_id`, `round_id`) VALUES (30.4, 3, 2);
+INSERT INTO `solves` (`time`, `user_id`, `round_id`) VALUES (90.9, 4, 2);
 
 INSERT INTO `friends` (`user_1_id`, `user_2_id`) VALUES (1, 2);
 INSERT INTO `friends` (`user_1_id`, `user_2_id`) VALUES (1, 3);
