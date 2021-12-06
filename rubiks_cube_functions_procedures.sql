8c5dba00282a6adf7a30fd7b1d9d8dcb962eb2b2
Graham Preston <graham.preston@gmail.com>
2021-12-06 13:21:27 -0500

    Move procedures_2 to other file

--- rubiks_cube_functions_procedures.sql
+++ rubiks_cube_functions_procedures.sql
@@ -1,4 +1,4 @@
-USE cubes1;
+USE cubes3;
 
 DROP FUNCTION IF EXISTS get_user_id;
 DELIMITER //
@@ -26,16 +26,32 @@ DELIMITER //
  END//
 DELIMITER ;
 
+DROP FUNCTION IF EXISTS get_cube_id;
+DELIMITER //
+CREATE FUNCTION get_cube_id(cube_name VARCHAR(100))
+RETURNS INT
+DETERMINISTIC
+READS SQL DATA
+BEGIN
+	DECLARE result INT;
+    SELECT cube_id INTO result FROM cube_types WHERE name = cube_name;
+    RETURN result;
+END //
+DELIMITER ;
+
 -- Procedure to find all the friends of a user
 DROP PROCEDURE IF EXISTS find_friends;
 DELIMITER //
-CREATE PROCEDURE find_friends(IN user_id int)
+CREATE PROCEDURE find_friends(IN uname VARCHAR(100))
 BEGIN
+DECLARE uid INT DEFAULT 0;
+SELECT get_user_id(uname) INTO uid;
+
 SELECT username FROM users
 JOIN (
-	(SELECT user_1_id AS this_user, user_2_id AS friend FROM friends WHERE user_1_id = user_id)
+	(SELECT user_1_id AS this_user, user_2_id AS friend FROM friends WHERE user_1_id = uid)
 	UNION
-	(SELECT user_2_id AS this_user, user_1_id AS friend FROM friends WHERE user_2_id = user_id)
+	(SELECT user_2_id AS this_user, user_1_id AS friend FROM friends WHERE user_2_id = uid)
 ) as t
 ON friend = users.user_id;
 END //
@@ -70,7 +86,7 @@ UPDATE sessions SET session_name = new_name WHERE session_id = (SELECT get_sessi
 END //
 DELIMITER ;
 
-CALL change_session_name("my_session", "session_1");
+-- CALL change_session_name("my_session", "session_1");
 
 -- Prodecure to add a penalty to a user's solve for a round
 DROP PROCEDURE IF EXISTS add_penalty;
@@ -81,6 +97,7 @@ UPDATE solves SET penalty = pen WHERE solve_id = (SELECT solve_id FROM solve_log
 END //
 DELIMITER ;
 
+-- Procedure to delete a user
 DROP PROCEDURE IF EXISTS remove_user;
 DELIMITER //
 CREATE PROCEDURE remove_user(IN uname VARCHAR(100))
@@ -120,9 +137,20 @@ DELIMITER ;
 -- Procedure to create a session
 DROP PROCEDURE IF EXISTS create_session;
 DELIMITER //
-CREATE PROCEDURE create_session(IN session_name VARCHAR(100), cube_id INT)
+CREATE PROCEDURE create_session(IN session_name VARCHAR(100), cube_name VARCHAR(100))
 BEGIN
+DECLARE cube_id INT;
+SELECT get_cube_id(cube_name) INTO cube_id;
 INSERT INTO sessions (`session_name`, `cube_id`) VALUES (session_name, cube_id);
+END //
+DELIMITER ;
+
+-- Procedure to list all sessions
+DROP PROCEDURE IF EXISTS list_sessions;
+DELIMITER //
+CREATE PROCEDURE list_sessions()
+BEGIN
+SELECT * FROM sessions;
 END //
 DELIMITER ;
 
@@ -135,14 +163,87 @@ INSERT INTO rounds (`scramble`, `session_id`) VALUES (scramble, session_id);
 END //
 DELIMITER ;
 
+-- Procedure to list all rounds in a session
+DROP PROCEDURE IF EXISTS list_rounds;
+DELIMITER //
+CREATE PROCEDURE list_rounds(IN sid int)
+BEGIN 
+SELECT * FROM rounds WHERE session_id = sid;
+END //
+DELIMITER ;
+
 -- Procedure to add a solve to a round
 DROP PROCEDURE IF EXISTS add_solve;
 DELIMITER //
 CREATE PROCEDURE add_solve(IN user_id INT, round_id INT, time FLOAT, penalty VARCHAR(10))
 BEGIN
-INSERT INTO solves (`time`, `penalty`) VALUES (time, penalty);
-INSERT INTO solve_logs (`solve_id`, `user_id`, `round_id`)
-	VALUES ((select max(solve_id) from solves), user_id, round_id);
+INSERT INTO solves (`time`, `penalty`, `user_id`, `round_id`) VALUES (time, penalty, user_id, round_id);
+END //
+DELIMITER ;
+
+-- Procedure to list all solves in a round
+DROP PROCEDURE IF EXISTS list_solves;
+DELIMITER //
+CREATE PROCEDURE list_solves(IN rid INT)
+BEGIN
+SELECT * FROM solves WHERE round_id = rid;
+END //
+DELIMITER ;
+
+-- Procedure to find all solves for a user grouped by cube type and ordered by time
+-- Returns round, time, penalty, and cube type
+DROP PROCEDURE IF EXISTS find_solves_for_user;
+DELIMITER //
+CREATE PROCEDURE find_solves_for_user(IN uname VARCHAR(100))
+BEGIN
+SELECT round_id, time, penalty, name 
+FROM (SELECT * FROM solves NATURAL JOIN solve_logs NATURAL JOIN rounds NATURAL JOIN
+(SELECT round_id, name FROM rounds NATURAL JOIN (SELECT session_id, name FROM sessions NATURAL JOIN cube_types) AS t) AS t2) AS t3
+WHERE user_id = (SELECT get_user_id(uname)) ORDER BY name, time ASC;
+END // 
+DELIMITER ;
+
+-- CALL find_solves_for_user("audrey");
+
+-- Procedure to calculate the current average of the last 5 time submitted by a user
+-- If user does not have 5 yet, averages all the scores
+DROP PROCEDURE IF EXISTS average_of_5;
+DELIMITER //
+CREATE PROCEDURE average_of_5(IN uname VARCHAR(100))
+BEGIN
+SELECT AVG(time) FROM solves NATURAL JOIN solve_logs 
+WHERE user_id = (SELECT get_user_id(uname)) ORDER BY solve_id DESC LIMIT 5;
+END //
+DELIMITER ;
+
+-- CALL average_of_5("audrey");
+
+-- Procedure to update a username
+DROP PROCEDURE IF EXISTS update_username;
+DELIMITER //
+CREATE PROCEDURE update_username(IN old_name VARCHAR(100), IN new_name VARCHAR(100))
+BEGIN
+UPDATE users SET username = new_name WHERE user_id = (SELECT get_user_id(old_name));
+END //
+DELIMITER ;
+
+-- CALL update_username("graham", "audreys_partner");
+
+-- Prodecure to delete a session--should also delete rounds and solve log entries
+DROP PROCEDURE IF EXISTS delete_session;
+DELIMITER //
+CREATE PROCEDURE delete_session(IN session_name VARCHAR(100))
+BEGIN
+DELETE FROM sessions WHERE session_id = (SELECT get_session_id(session_name));
+END //
+DELIMITER ;
+
+-- Procedure to delete a round--all solve log entries from that round will also be deleted
+DROP PROCEDURE IF EXISTS delete_round;
+DELIMITER //
+CREATE PROCEDURE delete_round(IN roundid INT)
+BEGIN
+	DELETE FROM rounds WHERE round_id = roundid;
 END //
 DELIMITER ;
 
